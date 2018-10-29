f6756fdb6684db619f796f845df154ad2a9ab182
Marty <marty.stepien@gmail.com>
2018-10-29 11:41:21 +0000

    Test driving a new class

--- models/person.js
+++ models/person.js
@@ -1,1 +1,16 @@
-// Your Person class goes here!
+class Person {
+    constructor(firstName, surname, dob) {
+        this.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+        this.surname = surname;
+        this.dob = dob;
+        this.emails = [];
+    }
+    fullname() {
+        return this.firstName + ' ' + this.surname;
+    }
+    addEmail(emailAddress) {
+        return this.emails.push(emailAddress);
+    }
+}
+
+module.exports = Person
---
+++ requirements.md
@@ -0,0 +1,6 @@
+# Person Project for Ada 
+
+- [x] Constructor. Check it worked.
+- [x] Capitalising first name.
+- [ ] Fullname method.
+- [ ] Emails attribute and addEmail.
